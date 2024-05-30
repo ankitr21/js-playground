226b1328923a50af9b138c19039b03f29d27881c
ankitr21 <akrranu@gmail.com>
2024-05-30 17:28:01 +0000

    conversions

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "hitesh"
+
+console.log(typeof score)
+
+let valueInnumber = Number(score)
+
+console.log(typeof valueInnumber)
+console.log(valueInnumber)
+
+// "33" -> 33
+// "33ads" -> NaN
+// true -> 1; false -> 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 -> true; 0 -> false
+// "" -> false
+// "abc" -> true
+
+let testnum = 33
+let numtostring = String(testnum)
+
+console.log(typeof numtostring)
+console.log(numtostring)
