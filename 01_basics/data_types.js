ea820863e6ff3f22b7d10163838a28c3d1c62b0e
ankitr21 <akrranu@gmail.com>
2024-05-30 17:17:16 +0000

    data_types and variables

---
+++ 01_basics/data_types.js
@@ -0,0 +1,18 @@
+let name = "Ankit"
+let age = 215
+let isLoggedIn = false
+
+
+//number => 2 ^ 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not assigned 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object
---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 23131
+let accountEmail = "äkrranu@gmail.com"
+
+var accountPass = "12345" //not prefered
+accountCity = "Patna" //not prefered
+
+//accountId = 35464
+accountEmail = "abc@gmail.com"
+accountPass = "454543"
+accountCity = "delhi"
+let accountState;
+
+/*
+prefer not to use var due to issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
