8ce255e44b4f595ae605957fa71ceaa40946ac42
ankitr21 <akrranu@gmail.com>
2024-06-01 05:00:58 +0000

    memory allocation

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -24,5 +24,32 @@ const myFunction = function() {
     console.log("Hello World");
 }
 
-console.log(typeof myObj);
-console.log(typeof myFunction);
+// console.log(typeof myObj);
+// console.log(typeof myFunction);
+
+
+// Memory Allocation
+
+// Stack (Primitive), Heap (Non - Primitive)
+
+
+let myYoutubeName = "hello channel"
+
+let anotherName = myYoutubeName
+
+// copy created so value will not change 
+console.log(myYoutubeName)
+console.log(anotherName)
+
+let user1 = {
+    name:"Ankit",
+    place:"Patna",
+}
+
+let user2 = user1
+
+user2.name = "Ranu"
+
+// value changed since reference is used
+console.log(user1)
+console.log(user2)
