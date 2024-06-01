002e97bbcdd677aa0242605329fabe8600ee638c
ankitr21 <akrranu@gmail.com>
2024-06-01 05:54:31 +0000

    maths and strings

---
+++ 01_basics/06_string.js
@@ -0,0 +1,20 @@
+const name = "Ankit"
+const repoCount = 40
+
+// console.log(name + repoCount + "Value");;
+
+// `` backticks is used for string interpolation. We can use place holders for variables and can also use any functions on them on the go
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Hello')
+
+// console.log(gameName[3])
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(2))
+
+// console.log(gameName.indexOf('o'))
+
+console.log(gameName.slice(-2,5))
---
+++ 01_basics/07_maths.js
@@ -0,0 +1,28 @@
+const score = 8900
+//console.log(score)
+
+const balance = new Number(700)
+//console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+let otherNumber = 123.1225
+//console.log(otherNumber.toPrecision(1))
+
+const temp = 1000000
+//console.log(temp.toLocaleString('en-IN'))
+
+// ****************** Maths **************************
+
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.round(9.3))
+
+console.log(Math.random().toFixed(2))
+
+const min = 10
+const max = 20
+
+// in Math.random 0 is inclusive 1 is exculsive
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
