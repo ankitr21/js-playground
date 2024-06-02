1300694052e3e5e0166f859282d0e8cfc069a2b9
ankitr21 <akrranu@gmail.com>
2024-06-02 10:10:01 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let i = 1
+while (i < 4) {
+    console.log(`value of i ${i}`)
+    i = i + 1;
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
